1031a4bebe1f063de12ed88235ad018a4a188444
vlad_izmailov <izmailov@miura.tech>
2021-09-28 00:13:38 +0300

    start configuration

---
+++ song/create.php
@@ -0,0 +1,68 @@
+<?php
+// необходимые HTTP-заголовки 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// получаем соединение с базой данных 
+include_once '../config/database.php';
+
+// создание объекта товара 
+include_once '../objects/song.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$song = new Song($db);
+ 
+// получаем отправленные данные 
+$data = json_decode(file_get_contents("php://input"));
+ 
+// убеждаемся, что данные не пусты 
+if (
+    !empty($data->name) &&
+    !empty($data->price) &&
+    !empty($data->description) &&
+    !empty($data->category_id)
+) {
+
+    // устанавливаем значения свойств товара 
+    $song->name = $data->name;
+    $song->price = $data->price;
+    $song->description = $data->description;
+    $song->category_id = $data->category_id;
+    $song->created = date('Y-m-d H:i:s');
+
+    // создание товара 
+    if($song->create()){
+
+        // установим код ответа - 201 создано 
+        http_response_code(201);
+
+        // сообщим пользователю 
+        echo json_encode(array("message" => "Товар был создан."), JSON_UNESCAPED_UNICODE);
+    }
+
+    // если не удается создать товар, сообщим пользователю 
+    else {
+
+        // установим код ответа - 503 сервис недоступен 
+        http_response_code(503);
+
+        // сообщим пользователю 
+        echo json_encode(array("message" => "Невозможно создать товар."), JSON_UNESCAPED_UNICODE);
+    }
+}
+
+// сообщим пользователю что данные неполные 
+else {
+
+    // установим код ответа - 400 неверный запрос 
+    http_response_code(400);
+
+    // сообщим пользователю 
+    echo json_encode(array("message" => "Невозможно создать товар. Данные неполные."), JSON_UNESCAPED_UNICODE);
+}
+?>
